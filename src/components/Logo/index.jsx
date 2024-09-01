bd6db0ce933105a6a5d9bedf926ed81d70e1c543
Thomgomes <math.011@hotmail.com>
2024-09-01 18:00:53 -0300

    alteração no diretorio da logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import logo from '../../assets/logoMini.png'
+import logo from '../../../public/LogoMini.png'
 
 export default function Logo() {
   return (
