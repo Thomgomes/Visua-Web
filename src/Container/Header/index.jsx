da1b9bc09f005c4d560f8f43ac3527ef50052d30
Thomgomes <math.011@hotmail.com>
2024-09-01 11:52:40 -0300

    erro no caminho da logo

--- src/Container/Header/index.jsx
+++ src/Container/Header/index.jsx
@@ -1,11 +1,11 @@
-import Logo from "../../components/Logo";
+// import Logo from "../../components/Logo";
 import Nav from "../../components/Nav";
 
 export default function Header() {
 
   return (
     <header className="top-0 h-32 z-[1] mx-auto flex w-full items-center justify-between border-b border-Visua-Grey p-8 md:p-16">
-        <Logo/>
+        {/* <Logo/> */}
         <Nav/>
     </header>
   )
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logoMini.png'
+// import { Link } from 'react-router-dom'
+// import logo from '../../assets/logoMini.png'
 
-export default function Logo() {
-  return (
-    <Link to="/HomePage">
-        <img src={logo} alt="Visua" />
-    </Link>
-  )
-}
+// export default function Logo() {
+//   return (
+//     <Link to="/HomePage">
+//         <img src={logo} alt="Visua" />
+//     </Link>
+//   )
+// }
